4ad5d65d5ae34dec07ffaab9f5be2eebdafb1074
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-01-25 09:28:27 +0100

    09h28
    ajout de pdo.php au fichier .gitignore

--- src/pdo.php
+++ src/pdo.php
@@ -1,7 +1,18 @@
 <?php
 
+/*on récupère les constantes de connexion définies dans connect.php*/
+require("connect.php");
+
+/*on prépare la connexion à la base de données*/
+define('SERVER',"localhost");
+define('USER',"root");
+define('PASSWD',"");
+define('BASE',"favoris");
+
+
 //connexion à la BDD
 $pdo = new PDO('mysql:host=localhost;dbname=favoris', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
 
 
 //affiche toutes les méthodes PDO
