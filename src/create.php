b4ab4bc3b1e7f66a14f3c2afa0a3a8a80e437d2d
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-01-30 16:14:53 +0100

    16h15 30janv2024
    ajouter fav

--- src/create.php
+++ src/create.php
@@ -2,36 +2,88 @@
 <?php
     include("header.php");
     include("pdo.php");
+
+    // Récupération des catégories :
+    $result_cat = $pdo->query("SELECT * FROM categorie");
+    $categories = $result_cat->fetchAll(PDO::FETCH_ASSOC);
+
+    // Récupération des domaines :
+    $result_dom = $pdo->query("SELECT * FROM domaine");
+    $domaines = $result_dom->fetchAll(PDO::FETCH_ASSOC);
+
+
     ?>
 
 
 
 
 <!-- Formulaire de saisie du nouveau favori -->
-<form action="" method="post">
-    <ul class="flex justify-center m-20">
-        <li>
-            <label>Nom du favori</label>
-            <input></input>
-        </li>
-        <li>
-            <label>Catégorie</label>
-            <input></input>
-        </li>
-        <li>
-            <label>Domaine</label>
-            <input></input>
-        </li>
-        <li>
-            <label>Adresse URL</label>
-            <input></input>
-        </li>
-    </ul>
-</form>
+<section class="flex-col justify-center border-solid border-black border-2 bg-green-400 m-6 rounded-2xl">
+    <h2 class="text-center text-4xl m-6 font-bold">Nouveau favori</h2>
+    <h3 class="text-center text-xl">Champs à renseigner :</h2>
+    <form action="" method="post" class="text-center">
+        <ul class="flex-col m-20">
+            <li class="flex-col m-2">
+                <label for="nom">Nom du favori</label>
+                <input type="text" id="nom" name="nom" class="rounded-md"></input>
+            </li>
+            <li class="flex-col m-2">
+                <label>Catégorie</label>
+                <select class="cursor-pointer hover:bg-slate-100 hover:shadow-xl rounded-lg" id="cat" name="cat">
+                    <option value="">-- Choisissez une catégorie --</option>
+                        <?php 
+                            foreach($categories as $categorie){
+                            ?>
+                            <option value="<?php echo $categorie['id_cat']?>"><?php echo $categorie['nom_cat']?></option>
+                            <?php
+                            }
+                        ?>
+                </select>
+            </li>
+            <li class="flex-col m-2">
+                <label>Domaine</label>
+                <select id="dom" name="dom" class="cursor-pointer  hover:bg-slate-100 hover:shadow-xl rounded-lg">
+                        <option value="">-- Choisissez un domaine --</option>
+                        <?php 
+                            foreach($domaines as $domaine){
+                            ?>
+                            <option value="<?php echo $domaine['id_dom']?>"><?php echo $domaine['nom']?></option>
+                            <?php
+                            }
+                        ?>
+                    </select>
+            </li>
+            <li class="flex-col m-2">
+                <label>Adresse URL</label>
+                <input type="url" id="url" name="url" class="rounded-md"></input>
+            </li>
+        </ul>
+        <div class="flex justify-center">
+            <button type="submit" value="envoyer" class="bg-red-500 text-2xl font-bold rounded-lg p-2 border-solid border-black border-2 mb-5">Envoyer</button>
+        </div>
+    </form>
+</section>
 
 
 
-<!-- <?php echo"date de création du favori"; ?> -->
+<?php
+
+    $nom = htmlspecialchars($_POST['nom']);
+    $cat = htmlspecialchars($_POST['cat']);
+    $url = htmlspecialchars($_POST['url']);
+    $dom = htmlspecialchars($_POST['dom']);
+
+    echo $nom." ".$cat." ".$url." ".$dom." ".date('Y-m-d');
+
+
+    $creer_favori = ("INSERT INTO favori (libelle, date_creation, url, id_dom)  VALUES('$nom', 'date('Y-m-d')', '$url', '$dom')");
+    $ajout_cat = ("INSERT INTO categorie VALUES('$cat');");
+    $result_fav = $pdo->prepare($creer_favori);
+    $creer_favori->execute();
+    
+    ?>
+
+
 
 
 
--- src/delete.php
+++ src/delete.php
@@ -5,7 +5,7 @@
 
 
 <?php
-    echo "<h2 class='text-3xl text-center m-40 font-bold'>Votre favori a bien été supprimé de la base de données.<br>Vous allez être redirigé dans 5 secondes...</h2>";
+    echo "<h2 class='text-3xl text-center m-40 font-bold'>Le favori a bien été supprimé de la base de données.<br>Vous allez être redirigé dans 5 secondes...</h2>";
     // echo "<div class='flex justify-center'><a href='index.php' class='text-2xl text-center m-20 font-bold bg-lime-400 p-3 rounded-xl'>Cliquez ici pour retourner dans vos favoris</a></div>";
     ?>
 <?php 
